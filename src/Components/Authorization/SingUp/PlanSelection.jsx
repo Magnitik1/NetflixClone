4c0a134289aa1222f67b9da4bf7b1375e5bac78a
Magnitik1 <alexalex23122004@gmail.com>
2023-09-14 00:14:11 +0200

    +
    css united

--- src/Components/Authorization/SingUp/PlanSelection.jsx
+++ src/Components/Authorization/SingUp/PlanSelection.jsx
@@ -1,38 +1,43 @@
-import React, { useState } from "react";
-import "./planSelectionInfo.css";
+import React, {useState} from 'react';
+import './planSelectionInfo.css';
 import { Link } from "react-router-dom";
 import { Radio } from "@mui/material";
 
-const PlanSelection = (props) => {
+const PlanSelectionInfo = (props) => {
   let [selectedValue, setSelectedValue] = useState("");
 
   return (
-    <div className="plan-selection-container">
-      <div className="plan-selection-header">
-        <p className="step-text">Step 3 of 3</p>
-        <p className="plan-title">Choose your plan</p>
-      </div>
-      <ul className="plan-list">
-        <li className="plan-item">
-          <span className="checkmark">&#10003;</span>
-          <span className="plan-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-          </span>
-        </li>
-        <li className="plan-item">
-          <span className="checkmark">&#10003;</span>
-          <span className="plan-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-          </span>
-        </li>
-        <li className="plan-item">
-          <span className="checkmark">&#10003;</span>
-          <span className="plan-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-          </span>
-        </li>
-      </ul>
-      1
+    <div className="centered-container">
+      <a className="header-title">FLIXUA</a>
+      <div className="plan-selection-container">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+          <path d="M6.875 21.875L15.625 30.625L33.125 11.875" stroke="#FFC107" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        <div className="plan-selection-header">
+          <p className="step-text">Step 2 of 3</p>
+          <p className="plan-title">Choose your plan</p>
+        </div>
+        <ul className="plan-list">
+          <li className="plan-item">
+            <span className="checkmark">&#10003;</span>
+            <span className="plan-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </span>
+          </li>
+          <li className="plan-item">
+            <span className="checkmark">&#10003;</span>
+            <span className="plan-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </span>
+          </li>
+          <li className="plan-item">
+            <span className="checkmark">&#10003;</span>
+            <span className="plan-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </span>
+          </li>
+        </ul>
+        1
       <Radio
         checked={selectedValue === "a"}
         onChange={() => {
@@ -101,8 +106,9 @@ const PlanSelection = (props) => {
           Next
         </button>
       </Link>
+      </div>
     </div>
   );
 };
 
-export default PlanSelection;
+export default PlanSelectionInfo;
--- src/Components/Authorization/SingUp/PlanSelectionInfo.jsx
+++ src/Components/Authorization/SingUp/PlanSelectionInfo.jsx
@@ -2,36 +2,41 @@ import React from 'react';
 import './planSelectionInfo.css';
 import { useNavigate } from 'react-router-dom';
 
-
 const PlanSelectionInfo = (props) => {
   let navigate = useNavigate();
   return (
-    <div className="plan-selection-container">
-      <div className="plan-selection-header">
-        <p className="step-text">Step 2 of 3</p>
-        <p className="plan-title">Choose your plan</p>
+    <div className="centered-container">
+      <a className="header-title">FLIXUA</a>
+      <div className="plan-selection-container">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+          <path d="M6.875 21.875L15.625 30.625L33.125 11.875" stroke="#FFC107" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        <div className="plan-selection-header">
+          <p className="step-text">Step 2 of 3</p>
+          <p className="plan-title">Choose your plan</p>
+        </div>
+        <ul className="plan-list">
+          <li className="plan-item">
+            <span className="checkmark">&#10003;</span>
+            <span className="plan-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </span>
+          </li>
+          <li className="plan-item">
+            <span className="checkmark">&#10003;</span>
+            <span className="plan-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </span>
+          </li>
+          <li className="plan-item">
+            <span className="checkmark">&#10003;</span>
+            <span className="plan-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </span>
+          </li>
+        </ul>
+        <button className="sign-in-button1" onClick={() => navigate('/PlanSelection')}>Next</button>
       </div>
-      <ul className="plan-list">
-        <li className="plan-item">
-          <span className="checkmark">&#10003;</span>
-          <span className="plan-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-          </span>
-        </li>
-        <li className="plan-item">
-          <span className="checkmark">&#10003;</span>
-          <span className="plan-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-          </span>
-        </li>
-        <li className="plan-item">
-          <span className="checkmark">&#10003;</span>
-          <span className="plan-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-          </span>
-        </li>
-      </ul>
-      <button className="sign-in-button1" onClick={()=>navigate('/PlanSelection')}>Next</button>
     </div>
   );
 };
